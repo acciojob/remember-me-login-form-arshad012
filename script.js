100f1ad43d0f67de85ecb4845029d259c1eeb2ca
arshad012 <forarshad99@gmail.com>
2024-11-22 18:14:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const name = form.username.value;
+	const password = form.password.value;
+	const checked = form.checkbox.checked;
+
+	const userData = {name, password};
+	if(checked) {
+		localStorage.setItem('users', JSON.stringify(userData));
+	}
+	
+
+	const res = checkExistingUser(userData);
+	if(!res) {
+		alert(`Logged in as ${name}`);
+	}
+})
+
+function checkExistingUser(userData) {
+	const existingData = JSON.parse(localStorage.getItem('users'));
+	if(existingData && (existingData.name == userData.name) && (existingData.password == userData.password)) {
+		const existUserBtn = document.createElement('button');
+		existUserBtn.innerText = 'Login as existing user.';
+		existUserBtn.id = 'existing';
+		existUserBtn.onclick = () => {
+			alert(`Logged in as ${existingData.name}`);
+		}
+		
+		const body = document.querySelector('body');
+		// body.removeChild(existUserBtn);
+		body.append(existUserBtn);
+
+		return true;
+	}
+	return false;
+}
+
+checkExistingUser();
